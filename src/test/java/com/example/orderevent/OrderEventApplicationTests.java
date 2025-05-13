a0e6be7d855b9012ede386f3357f4396af8994f0
Blue1977 <hector.poncedeleon@utec.edu.pe>
2025-05-13 16:45:11 -0500

    Resolviendo conflictos

---
+++ src/test/java/com/example/orderevent/OrderEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
